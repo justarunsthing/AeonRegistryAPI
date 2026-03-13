201ae0f757fff2fc3e84f2927666f0023e10b333
justarunsthing <justarunsthing@outlook.com>
2026-03-13 08:40:27 +0000

    Added HomeEndpoints class & extension method

---
+++ Endpoints/Home/HomeEndpoints.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace AeonRegistryAPI.Endpoints.Home
+{
+    public static class HomeEndpoints
+    {
+        public static IEndpointRouteBuilder MapHomeEndpoints(this IEndpointRouteBuilder route)
+        {
+            var homeGroup = route.MapGroup("/api/Home")
+                .WithTags("Home");
+
+            // /api/Home/welcome
+            homeGroup.MapGet("/welcome", GetWelcomeMessage)
+                .WithName("GetWelcomeMessage")
+                .WithSummary("Welcome message")
+                .WithDescription("Displays a welcome message");
+
+            return route;
+        }
+
+        // Route handlers
+        private static async Task<Ok<WelcomeResponse>> GetWelcomeMessage(CancellationToken token)
+        {
+            var response = new WelcomeResponse
+            {
+                Message = "Welcome to the Aeon Registry API",
+                Version = "1.0.0",
+                TimeOnly = DateTime.Now.ToString("T")
+            };
+
+            return TypedResults.Ok(response);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using AeonRegistryAPI.Endpoints.Home;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,17 +18,6 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.MapGet("/api/welcome", () =>
-{
-    var response = new
-    {
-        Message = "Welcome to the Aeon Registry API",
-        Version = "1.0.0",
-        TimeOnly = DateTime.Now.ToString("T")
-    };
-
-    return Results.Ok(response);
-})
-.WithName("Welcome");
+app.MapHomeEndpoints();
 
 app.Run();
