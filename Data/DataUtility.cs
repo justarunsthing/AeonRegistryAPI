b6ae49e0a02c8090a0f6dde8674e07072a84b46d
justarunsthing <justarunsthing@outlook.com>
2026-03-14 09:56:36 +0000

    Added DataUtility class

---
+++ Data/DataUtility.cs
@@ -0,0 +1,12 @@
+﻿namespace AeonRegistryAPI.Data
+{
+    public static class DataUtility
+    {
+        public static string GetConnectionstring(IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DbConnection");
+
+            return connectionString!;
+        }
+    }
+}
