ffb590df02a4b18c5884b3f03b9c53b8ec27ff33
justarunsthing <justarunsthing@outlook.com>
2026-03-15 10:37:49 +0000

    Added BlockIdentityEndpoints class

---
+++ Middleware/BlockIdentityEndpoints.cs
@@ -0,0 +1,32 @@
+﻿namespace AeonRegistryAPI.Middleware
+{
+    public class BlockIdentityEndpoints(RequestDelegate next)
+    {
+        private static readonly string[] BlockedPaths = 
+        [
+            "/api/auth/register",
+            "/api/auth/resetpassword",
+            "/api/auth/forgotpassword",
+            "/api/auth/manage/info",
+            "/api/auth/manage/profile"
+        ];
+
+        private readonly RequestDelegate _next = next;
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var path = context.Request.Path.Value?.ToLowerInvariant();
+
+            if (path is not null && BlockedPaths.Contains(path))
+            {
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+
+                await context.Response.WriteAsync("Not Found!");
+
+                return;
+            };
+
+            await _next(context);
+        }
+    }
+}
