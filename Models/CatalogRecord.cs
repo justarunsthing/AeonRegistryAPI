942526311df0f5b319e58f83200489e9cdb928ae
justarunsthing <justarunsthing@outlook.com>
2026-03-27 07:53:54 +0000

    Added CatalogRecord class

---
+++ Models/CatalogRecord.cs
@@ -0,0 +1,27 @@
+﻿using AeonRegistryAPI.Enums;
+
+namespace AeonRegistryAPI.Models
+{
+    public class CatalogRecord
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Status { get; set; } = CatalogStatus.Draft.ToString();
+
+        [Required]
+        public DateTime DateSubmitted { get; set; } = DateTime.UtcNow;
+
+        // Navigational properties
+        [Required]
+        public int ArtifactId { get; set; }
+        public Artifact Artifact { get; set; } = null!;
+
+        [Required]
+        public string SubmittedById { get; set; } = string.Empty;
+        public ApplicationUser SubmittedBy { get; set; } = null!;
+        public string? VerifiedById { get; set; }
+        public ApplicationUser? VerifiedBy { get; set; } = null!;
+        public ICollection<CatalogNote> Notes { get; set; } = [];
+    }
+}
