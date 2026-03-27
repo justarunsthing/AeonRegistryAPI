f161cd63fbdacb3aa42c4c3e66b337364973f26f
justarunsthing <justarunsthing@outlook.com>
2026-03-27 08:02:10 +0000

    Added DbSet for models

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,6 +5,10 @@ namespace AeonRegistryAPI.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
-
+        public DbSet<Artifact> Artifacts { get; set; }
+        public DbSet<Site> Sites { get; set; }
+        public DbSet<ArtifactMediaFile> ArtifactMediaFiles { get; set; }
+        public DbSet<CatalogRecord> CatalogRecords { get; set; }
+        public DbSet<CatalogNote> CatalogNotes { get; set; }
     }
 }
