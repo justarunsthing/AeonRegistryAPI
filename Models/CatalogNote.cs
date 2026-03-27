b1ea4bfba6bec243dfbe1f17ec5e9a5f6b8bba5a
justarunsthing <justarunsthing@outlook.com>
2026-03-27 07:52:58 +0000

    Added CatalogNote class

---
+++ Models/CatalogNote.cs
@@ -0,0 +1,20 @@
+﻿namespace AeonRegistryAPI.Models
+{
+    public class CatalogNote
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(1000)]
+        public string Content { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+
+        // Navigational Properties
+        [Required]
+        public int CatalogRecordId { get; set; }
+        public CatalogRecord? CatalogRecord { get; set; } = null!;
+        public string AuthorId { get; set; } = string.Empty;
+        public ApplicationUser? Author { get; set; } = null!;
+    }
+}
