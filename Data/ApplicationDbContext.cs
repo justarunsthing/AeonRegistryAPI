0a6e97339c2b8bab12250f77c28d26d8f4d108e1
justarunsthing <justarunsthing@outlook.com>
2026-03-28 09:24:06 +0000

    Define relationships, FK & conversions

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,5 +10,31 @@ namespace AeonRegistryAPI.Data
         public DbSet<ArtifactMediaFile> ArtifactMediaFiles { get; set; }
         public DbSet<CatalogRecord> CatalogRecords { get; set; }
         public DbSet<CatalogNote> CatalogNotes { get; set; }
+
+        // Customize EF Core's default behaviour - defining relationships, FKs & conversions
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // Set up all necessary configs
+            base.OnModelCreating(builder);
+
+            // Custom mappings
+            // DeleteBehavior.Restrict prevents deleting a user if they have associated submissions
+            builder.Entity<CatalogRecord>()
+                .HasOne(cr => cr.SubmittedBy)
+                .WithMany(u => u.SubmittedCatalogRecords)
+                .HasForeignKey(cr => cr.SubmittedById)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<CatalogRecord>()
+                .HasOne(cr => cr.VerifiedBy)
+                .WithMany(u => u.VerifiedCatalogRecords)
+                .HasForeignKey(cr => cr.VerifiedById)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Store the enum as its string name
+            builder.Entity<Artifact>()
+                .Property(a => a.Type)
+                .HasConversion<string>();
+        }
     }
 }
