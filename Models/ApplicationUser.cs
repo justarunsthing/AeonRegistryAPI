4b98ab45d42838fc6cfbda330ca31c97981c5dbd
justarunsthing <justarunsthing@outlook.com>
2026-03-28 09:23:43 +0000

    Added navigational properties

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -12,5 +12,10 @@ namespace AeonRegistryAPI.Models
         public string? LastName { get; set; }
 
         public string FullName => $"{FirstName} {LastName}";
+
+        // Navigational Properties
+        public ICollection<CatalogRecord> SubmittedCatalogRecords { get; set; } = [];
+        public ICollection<CatalogRecord> VerifiedCatalogRecords { get; set; } = [];
+        public ICollection<ArtifactMediaFile> UploadedMedia { get; set; } = [];
     }
 }
