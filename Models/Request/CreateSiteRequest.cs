37803fa41e5e36c73265714e492b8c2c75c84e92
justarunsthing <justarunsthing@outlook.com>
2026-04-07 08:12:10 +0100

    Added CreateSiteRequest model

---
+++ Models/Request/CreateSiteRequest.cs
@@ -0,0 +1,25 @@
+﻿namespace AeonRegistryAPI.Models.Request
+{
+    public class CreateSiteRequest
+    {
+        [Required, MaxLength(200)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, MaxLength(100)]
+        public string Location { get; set; } = string.Empty;
+
+        [MaxLength(200)]
+        public string? Description { get; set; }
+
+        [MaxLength(100)]
+        public string? Coordinates { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        [MaxLength(2000)]
+        public string? PublicNarrative { get; set; }
+
+        [MaxLength(2000)]
+        public string? InternalNarrative { get; set; }
+    }
+}
